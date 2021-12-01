ceafe16f9dda35041d8f65b02656784c9240fc16
Eddishot <Eddi_sl94@hotmail.it>
2021-12-01 23:37:47 +0100

    milestone 1 completed

---
+++ components/logo.php
@@ -0,0 +1,7 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <img class="navbar-brand" src="https://loghi-famosi.com/wp-content/uploads/2020/09/Logo-della-Spotify.png">
+        </div>
+</nav>
--- index.php
+++ index.php
@@ -10,22 +10,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <title>Ajax Dischi Spotify</title>
 </head>
 <body>
-    <div>
-        <?php
-        foreach($albums as $element ){
-            echo $element ["title"];
-            echo $element ["author"];
-            echo $element ["genre"];
-            echo $element ["year"];
-            // foreach($singleAlbum as $element){
-            //     echo $element;
-            // }
-            // var_dump($element);
-        }
-        ?>
-    </div>
+    <header>
+    <?php
+        require __DIR__ . "/components/logo.php";
+    ?>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row row-cols-5">
+                <?php
+                    foreach($albums as $element ){
+                        // aggiungo html intestato al php
+                    // echo $element ["poster"];
+                    // echo $element ["title"];
+                    // echo $element ["author"];
+                    // echo $element ["year"];
+                    // foreach($singleAlbum as $element){
+                    //     echo $element;
+                    // }
+                    // var_dump($element);
+                    require __DIR__ . "/components/singleCard.php";
+                }?>
+                
+            </div>
+       
+        </div>
+    </main>
 </body>
 </html>
